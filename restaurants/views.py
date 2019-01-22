c19659414ecc37c68a325b4cfc5b00c9d8fdf18f
lilyBrainy <m.o.kh.alenzii@gmail.com>
2019-01-22 18:45:23 +0300

    Edited

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+
 
 # Create your views here.
+context ={
+	"msg": "Hello World!"
+}
+def home(request):
+	return render(request, 'taskTwo.html' , context)
